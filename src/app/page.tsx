fbc59244fbd0a79159a83c5003cbbbd10dd63cbb
mayankse <mayank.setiya@gmail.com>
2024-01-06 16:12:46 +0530

    update1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
 import AboutPage from './about/page';
+import HeaderPage from './header/page';
+
 export default function Home() {
   return (<div>
     <AboutPage/>
+    <HeaderPage/>
   </div>);
 }
